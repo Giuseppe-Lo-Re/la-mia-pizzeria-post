b089131fe8e3d0a8efebb9f874664e566c8b233c
Giuseppe Lo Re <loregiuseppe0@gmail.com>
2022-11-30 13:36:23 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+            List<Pizza> listPizzas = db.Pizzas.ToList();
+
+            return View(listPizzas);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
