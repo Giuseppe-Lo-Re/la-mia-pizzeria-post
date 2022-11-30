7f4050bd23a02cf9e8bf61e321d518a476191eb7
Giuseppe Lo Re <loregiuseppe0@gmail.com>
2022-11-30 17:24:52 +0100

    create validazioni

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,15 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il titolo non può essere più lungo di 50 caratteri")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(200, ErrorMessage = "La descrizione non può essere più lunga di 200 caratteri")]
         [Column(TypeName = "text")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         public string Image { get; set; }
         public float Price { get; set; }
 
@@ -19,6 +26,11 @@ namespace la_mia_pizzeria_static.Models
             Description = description;
             Image = image;
             Price = price;
+        }
+
+        public Pizza()
+        {
+
         }
     }
 }
